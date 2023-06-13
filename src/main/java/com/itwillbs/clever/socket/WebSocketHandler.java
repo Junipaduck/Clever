0090cba3b98da2fd85abfcd6c16b486472ed9760
choib0 <choi06029@gmail.com>
2023-06-13 22:59:36 +0900

    0613-최보아-채팅수정

--- src/main/java/com/itwillbs/clever/socket/WebSocketHandler.java
+++ src/main/java/com/itwillbs/clever/socket/WebSocketHandler.java
@@ -31,56 +31,6 @@ public class WebSocketHandler extends TextWebSocketHandler implements Initializi
 	@Autowired
 	private ChattingController chattingController;
 
-//	private List<WebSocketSession> users;
-//	private Map<String, Object> userMap;
-//	
-//	public WebSocketHandler() {
-//		users = new ArrayList<WebSocketSession>();
-//		userMap = new HashMap<String, Object>();
-//	}
-//	
-//	// 소켓 연결
-//	@Override
-//	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-//		System.out.println("chatting - WebSocket : 연결 성공");
-//		users.add(session);
-//	}
-//	
-//	
-//	// 채팅 메세지 수신 후 실행 메서드
-//	@Override
-//	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//		
-//		System.out.println("chatting - WebSocket : 메세지 수신");
-//		System.out.println("메세지 : " + message.getPayload());
-//		
-//		JSONObject object = new JSONObject(message.getPayload());
-//		String type = object.getString("type");
-//		
-//		if(type != null && type.equals("register")) {
-//			// 등록 요청 메세지
-//			String user = object.getString("userId");
-//			// 아이디랑 session 매핑
-//			userMap.put(user, session);
-//		} else {
-//			// 채팅 메세지 : 상대방 아이디 포함해서 메세지 보내짐
-//			// Map에서 상대방 아이디에 해당하는 WebSocket 꺼내와서 메세지 전송
-//			String target = object.getString("target");
-//			WebSocketSession ws = (WebSocketSession)userMap.get(target);
-//			String msg = object.getString("message");
-//			if(ws != null) {
-//				ws.sendMessage(new TextMessage(msg));
-//			}
-//		}
-//		
-//	}
-//	
-//	// 연결 해제 후 실행 메서드
-//	@Override
-//	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
-//		System.out.println("TextWebSocketHandler : 연결 종료!");
-//		users.remove(session);
-//	}
 	
 	private final ObjectMapper objectMapper = new ObjectMapper();
 	private Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<WebSocketSession>());
@@ -99,31 +49,8 @@ public class WebSocketHandler extends TextWebSocketHandler implements Initializi
 		
 	}
 	
-//	// Handler가 받은 메세지
-//	@Override
-//	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-//		
-//		// 세션아이디때문에 추가 - 0530
-//		String senderId = (String)session.getAttributes().get("sessionId");
-//		logger.info(senderId);
-//		
-//		
-//		logger.info("#WebSocketHandler, handleMessage");
-//		// session.getId() : 사용자 아이디
-//		// message.getPayload() : 메세지 내용
-//		logger.info(session.getId() + ": " + message.getPayload());
-//		
-//		System.out.println(sessionList);
-//		
-////		sessionList.get(0).sendMessage(message);
-//		
-//		// 접속된 모든 이용자들에게 메세지를 날림 (브로드캐스팅)
-//		for(WebSocketSession s : sessionList) {
-//			System.out.println(s);
-//			s.sendMessage(new TextMessage(session.getId() + ":" + message.getPayload()));
-//		}
-//	}
 	
+	// 메세지 전달되었을 때
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception{
 		
@@ -143,8 +70,6 @@ public class WebSocketHandler extends TextWebSocketHandler implements Initializi
 	    String sellerId = jo.getString("seller_id");
 	    String senderId = jo.getString("senderId");
 	    
-//	    List<ChatRoomVO> selectChatRoom = chattingService.selectChatRoom(chatRoomId);
-//	    System.out.println("앜" + selectChatRoom);
 	    List<ChatRoomVO> selectChatRoom = chattingService.selectChatRoom(senderId, productIdx);
 	    System.out.println("앜" + selectChatRoom); 
 	    
@@ -164,30 +89,6 @@ public class WebSocketHandler extends TextWebSocketHandler implements Initializi
 	    if (result > 0) {
 	    	System.out.println("채팅 메세지 저장");
 	    }
-	    
-	    
-	    
-//	    chatMessage.setCreateDate(new Date(System.currentTimeMillis()));
-//	    logger.info(chatMessage);
-//
-//	    // 전달 받은 채팅메세지를 db에 삽입
-//	    int result = chatService.insertMessage(chatMessage);
-//	    if(result > 0) {
-//	        // 같은방에 접속중인 클라이언트에게 전달받은 메세지를 보내기
-//	        for(WebSocketSession s : sessions) {
-//	            //반복을 진행중인 websocketSession안에 담겨있는 방번호
-//	            int chatRoomNo = (Integer)s.getAttributes().get("chatRoomNo");
-//
-//	            //메세지에 담겨있는 채팅방 번호와 chatRoomNo가 같은지 비교
-//	            if(chatMessage.getChatRoomNo() == chatRoomNo) {
-//	                //같은 방 클라이언트에게 JSON형식으로 메세지를 보냄
-//
-//	                // s.sendMessage(new TextMessage( message.getPayload()));
-//	                s.sendMessage(new TextMessage( new Gson().toJson(chatMessage )));
-//	            }
-//	        }
-//
-//	    }
 	}
 	
 	
